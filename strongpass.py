3d124987b57ff04491173d2d420350bd25646143
BrainfuckSec <brainfuck@protonmail.ch>
2017-02-05 17:39:47 +0100

    New module secrets instead random

--- strongpass.py
+++ strongpass.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Program: strongpass.py
-# Version: 3.2.1 
+# Version: 3.3.0
 # Description: Generate random passwords 
-# Dev: Brainfuck 
+# 
+# Copyright (C) 2015, 2016. 2017 Brainfuck 
 
 # GNU GENERAL PUBLIC LICENSE
 # This program is free software: you can redistribute it and/or modify
@@ -22,74 +23,75 @@
 import sys
 import argparse
 import string
-import random 
+import secrets
 
-# program version
-version = "3.2.1"
+# program name and version
+_PROGRAM = "strongpass.py"
+_VERSION = "3.3.0"
+
 
 # banner
 def banner():
-	print("strongpass.py %s\n" % (version))
-	print("Generate random passwords\n")
+    print("{} {}\n".format(_PROGRAM, _VERSION))
+    print("Generate random passwords\n")
 
 
 # print version and exit
-def prog_version():
-	banner()
-	print("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>")
-	print("This is free software: you are free to change and redistribute it.")
-	print("There is NO WARRANTY, to the extent permitted by law.")
-	exit(0)
+def print_version():
+    banner()
+    print("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>")
+    print("This is free software: you are free to change and redistribute it.")
+    print("There is NO WARRANTY, to the extent permitted by law.")
+    exit(0)
 
 
 # function for check the user input 
 def check(input_args):
-	input_args = int(input_args)
-	if input_args > 254:
-		raise argparse.ArgumentTypeError("please enter an integer no more bigger than 254")
-		exit(1)
-	else:
-		return input_args
+    input_args = int(input_args)
+    if input_args > 254:
+        raise argparse.ArgumentTypeError("please enter an integer no more bigger than 254")
+        exit(1)
+    else:
+        return input_args
 
 
 # main
 def main():
-	parser = argparse.ArgumentParser(
-	prog = "strongpass.py",
-	formatter_class = argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+    formatter_class = argparse.RawDescriptionHelpFormatter)
 
-	# type=check --> def check()
-	parser.add_argument("-l", "--lenght", type=check, help="Lenght of password")
-	parser.add_argument("-n", "--number", type=check, help="Number of password to generate")
-	parser.add_argument("-v", "--version", action="store_true", help="display program version and exit")
+    # type=check --> def check()
+    parser.add_argument("-l", "--lenght", type=check, help="Lenght of password")
+    parser.add_argument("-n", "--number", type=check, help="Number of password to generate")
+    parser.add_argument("-v", "--version", action="store_true", help="display program version and exit")
 
-	args = parser.parse_args()
-	
-	# if no arguments are given, print banner and help
-	if len(sys.argv) == 1:
-		banner()
-		parser.print_help()
-		exit(1)
+    args = parser.parse_args()
+    
+    # if no arguments are given, print banner and help
+    if len(sys.argv) == 1:
+        banner()
+        parser.print_help()
+        exit(1)
 
-	if args.version:
-		prog_version()
-	
-	# generate password/s
-	if args.lenght and args.number:
-		for x in range(args.number):
-			# charset defined with variables 
-			# chars excluded --> l0Oo'`ìèéòçà°
-			letters = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ"
-			numbers = "123456789"
-			special = "|\!$%&/()=?^~[]{}+*@#<>,;.:-_"				
-			chars = letters + numbers + special
-			password = "".join((random.choice(chars)) for x in range(args.lenght))
-			print(password)	
-	else:
-		parser.error("the following arguments are required:: -l, --lenght <lenght> -n, --number <number>")
-		exit(1)
+    if args.version:
+        print_version()
+    
+    """ generate password/s
+    charset are defined with variables
+    characters excluded: l0Oo`ìèéòçà°
+    """
+    if args.lenght and args.number:
+        for x in range(args.number):
+            letters = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ"
+            numbers = "123456789"
+            special = "|\!$%&/()='?^~[]{}+*@#<>,;.:-_"               
+            chars = letters + numbers + special
+            password = "".join((secrets.choice(chars)) for x in range(args.lenght))
+            print(password) 
+    else:
+        parser.error("the following arguments are required:: -l, --lenght <lenght> -n, --number <number>")
+        exit(1)
 
 
 if __name__ == "__main__":
-	main()
-	
+    main()
