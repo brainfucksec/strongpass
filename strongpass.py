c80e05bd064bc60002124b09f2c507fadb2b03be
brainfucksec <brainfuck@protonmail.ch>
2017-08-24 12:29:25 +0200

    version: 3.5-git

--- strongpass.py
+++ strongpass.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 # Program: strongpass.py
-# Version: 3.4.2
-# Description: Generate strong random passwords 
-# 
-# Copyright (C) 2015-2017 Brainfuck 
+# Version: 3.5
+# Description: Generate strong random passwords
+#
+# Copyright (C) 2015-2017 Brainfuck
 
 # GNU GENERAL PUBLIC LICENSE
 # This program is free software: you can redistribute it and/or modify
@@ -20,7 +20,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" For generate passwords, used new module 'secrets' instead of random, 
+# program informations
+__program__ = "strongpass"
+__version__ = "3.5"
+__author__ = "Brainfuck"
+
+""" For generate passwords, used new module 'secrets' instead of random,
 so we have more entropy
 https://docs.python.org/3/library/secrets.html
 """
@@ -30,17 +35,11 @@ import string
 import secrets
 
 
-# program informations
-__program__ = "strongpass.py"
-__version__ = "3.4.2"
-__author__ = "Brainfuck"
-
-
 # banner
 def banner():
     print("{} {}".format(__program__, __version__))
     print("Generate strong random passwords")
-    print("Author: {}\n".format(__author__))
+    print("Copyright (C) 2015-2017 {}\n".format(__author__))
 
 
 # print version and exit
@@ -52,7 +51,7 @@ def print_version():
     sys.exit(0)
 
 
-"""Function for check the user input, raise an error if user put one integer 
+"""Function for check the user input, raise an error if user put one integer
 greather than 254
 """
 def check(input_args):
@@ -64,61 +63,81 @@ def check(input_args):
         return input_args
 
 
-# main function
-def main():
+# Get command line arguments with argparse
+def get_arguments():
     parser = argparse.ArgumentParser(
     formatter_class = argparse.RawDescriptionHelpFormatter)
+
     # type=check --> recall def check() function above
-    parser.add_argument("-l", "--lenght", type=check, 
-        help="Lenght of password")
-    parser.add_argument("-n", "--number", type=check, 
-        help="Number of password to generate")
-    parser.add_argument("-a", "--algorithm", choices=['1', '0'], 
-        help="\n1 - random password, 0 - pronounceable password")
-    parser.add_argument("-v", "--version", action="store_true", 
+    parser.add_argument("-l", "--lenght", type=check,
+        help="Lenght of passwords")
+    parser.add_argument("-n", "--number", type=check,
+        help="Number of passwords to generate")
+    parser.add_argument("-m", "--mode", choices=["0", "1", "2"],
+        help="Passwords generation mode: 0 only letters, 1 random, 2 alphanumeric")
+    parser.add_argument("-v", "--version", action="store_true",
         help="display program version and exit")
 
     args = parser.parse_args()
-    
+
+    # -v, --version
+    if args.version:
+        print_version()
+
     # if no arguments are given, print banner and help
     if len(sys.argv) == 1:
         banner()
         parser.print_help()
         sys.exit(1)
 
-    if args.version:
-        print_version()
-    
-    """ Generate password/s with "secrets", characters excluded: "l0Oo`ìèéòçà°"
+    # if one argument is missing print usage, error type and exit
+    # avoid 'required=True' flag
+    if not args.lenght and args.number and args.mode:
+        parser.print_usage()
+        print("error: the following arguments are required: -l LENGHT, -n NUMBER, -m {0,1,2}")
+        sys.exit(1)
+
+    return args
+
+
+# Generate password-s
+def gen_password():
+    args = get_arguments()
+
+    """characters excluded: "l0Oo`ìèéòçà°"
     TODO: add option to argparse for do this
     """
-    if args.lenght and args.number and args.algorithm:      
-        for x in range(args.number):
-            # charset parameters
-            letters = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ"
-            numbers = "123456789"
-            special = "|\!$%&/()='?^~[]{}+*@#<>,;.:-_"
-            # 1 = random passwords
-            if args.algorithm == "1":              
-                charset = letters + numbers + special
-                password = "".join((secrets.choice(charset)) 
-                    for x in range(args.lenght))
-                # add more entropy: at least one of all characters available
-                if (any((letters) for c in password)
-                        and any((numbers) for c in password)
-                        and any((special) for c in password)):
-                    print(password)
-            # 0 = pronounceable passwords
-            elif args.algorithm == "0":
-                charset = letters
-                password = "".join((secrets.choice(charset)) 
-                    for x in range(args.lenght))
-                print(password)
-    else:
-        parser.error("""
-The following arguments are required: 
--l, --lenght <lenght> | -n, --number <number> | -a --algorithm {1, 0}""")
-        sys.exit(1)
+    letters = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ"
+    numbers = "123456789"
+    special = "|\!$%&/()='?^~[]{}+*@#<>,;.:-_"
+
+    for x in range(args.number):
+        ## Mode
+        # 0 = only letters passwords
+        if args.mode == "0":
+            charset = letters
+
+        # 1 = random passwords
+        elif args.mode == "1":
+            charset = letters + numbers + special
+
+        # 2 = alphanumeric passwords
+        elif args.mode == "2":
+            charset = letters + numbers
+
+        # print password-s
+        password = "".join((secrets.choice(charset))
+            for x in range(args.lenght))
+        # add more entropy: at least one of all characters available
+        if (any((letters) for c in password)
+                and any((numbers) for c in password)
+                and any((special) for c in password)):
+            print(password)
+
+
+# main function
+def main():
+    gen_password()
 
 
 if __name__ == "__main__":
